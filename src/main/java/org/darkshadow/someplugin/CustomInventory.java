cbaa0805834b242583365458df277752a10ec835
Nick Helmers <128245445+nicktrovert@users.noreply.github.com>
2023-10-07 15:28:43 +0200

    Added a method to create custom items for the custom inventory

--- src/main/java/org/darkshadow/someplugin/CustomInventory.java
+++ src/main/java/org/darkshadow/someplugin/CustomInventory.java
@@ -3,13 +3,25 @@ package org.darkshadow.someplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class CustomInventory {
     public static Inventory customInventory = Bukkit.createInventory(null, 27, "§eMy Custom Inventory");
     static {
-        customInventory.setItem(9, new ItemStack(Material.ICE, 1));
-        customInventory.setItem(13, new ItemStack(Material.FEATHER, 1));
-        customInventory.setItem(17, new ItemStack(Material.BRICK, 1));
+        customInventory.setItem(9, CreateItem(Material.ICE, "§Toggle Freeze", 1 ,0));
+        customInventory.setItem(13, CreateItem(Material.FEATHER, "§Toggle Fly", 1, 0));
+        customInventory.setItem(17, CreateItem(Material.BRICKS, "§4Toggle Build", 1, 0));
+    }
+
+    public static ItemStack CreateItem(Material material, String name, int amount , int custommodeldata){
+        ItemStack itemStack = new ItemStack(material, amount);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(name);
+        itemMeta.addItemFlags(ItemFlag.HIDE_ITEM_SPECIFICS, ItemFlag.HIDE_ATTRIBUTES);
+        itemMeta.setCustomModelData(custommodeldata);
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
     }
 }
