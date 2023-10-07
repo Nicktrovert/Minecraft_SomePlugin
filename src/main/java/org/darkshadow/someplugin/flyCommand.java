0cac55a60983928557be221a26e846c8451a01ec
Nick Helmers <128245445+nicktrovert@users.noreply.github.com>
2023-10-07 17:17:39 +0200

    fixed fly command + build

--- src/main/java/org/darkshadow/someplugin/flyCommand.java
+++ src/main/java/org/darkshadow/someplugin/flyCommand.java
@@ -1,5 +1,6 @@
 package org.darkshadow.someplugin;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,7 @@ public class flyCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) commandSender;
         player.setAllowFlight(true);
+        player.teleport(player.getLocation().add(0, 0.5, 0));
         player.setFlying(true);
         return false;
     }
